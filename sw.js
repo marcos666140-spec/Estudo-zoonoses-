9d63fae1dd6f896e01302ff6ceeae66f503f99a2
marcos666140-spec <marcos666140@gmail.com>
2026-02-10 21:55:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = "estudos-v1";
-const FILES = [
-  "index.html",
-  "manifest.json",
-  "portugues/links.html",
-  "matematica/links.html",
-  "especificos/links.html"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    caches.open('estudos-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
